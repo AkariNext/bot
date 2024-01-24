caca2f4c2c0edf2acbc566bd9dd5c8f4224fcbe9
yupix <yupi0982@outlook.jp>
2024-01-24 14:04:33 +0900

    chore: run.py を分離

---
+++ run_bot.py
@@ -0,0 +1,4 @@
+from packages.bot import main as bot_app
+
+if __name__ == "__main__":
+    bot_app.run()
---
+++ run_web.py
@@ -0,0 +1,4 @@
+from packages.web import main as web_app
+
+if __name__ == "__main__":
+    web_app.run()
