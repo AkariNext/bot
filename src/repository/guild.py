fad3e84d9ab55789099c6a16d164c6e40827b9dc
yupix <yupi0982@outlook.jp>
2024-09-20 09:40:58 +0900

    feat: prismaとさよならバイバイ、俺はsqlmodelと旅に出る

--- src/repository/guild.py
+++
@@ -1,9 +0,0 @@
-from prisma.models import Guild
-
-class GuildRepository:
-    @staticmethod
-    async def create(discordId: int):
-        return await Guild.prisma().create({
-                'discordId': discordId
-        })
-
